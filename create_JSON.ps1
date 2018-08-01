541578ddff73e4c42ac78110fc41bd01288a04f4
Casey Salvador <salvador.caseychad@gmail.com>
2018-08-01 08:59:31 -0400

    create_JSON
    This allows you to create a simple JSON file thru PowerShell and save it anywhere you want. This also allows nested JSON.

---
+++ create_JSON.ps1
@@ -0,0 +1,29 @@
+﻿<########################################################################
+#########################################################################
+#                                                                       #
+#                              Create JSON                              #
+#                                                                       #
+#########################################################################
+#########################################################################
+#                                                                       #
+# Name:       create_JSON.ps1                                           #
+# Version:    1.0                                                       #
+# Author:     Casey Salvador                                            #
+# Created:    26 Jul 2018                                               #
+# Usage:      Creates a JSON file                                       #
+# Notes:      Must run script as Admin                                  #
+#########################################################################>
+$newJSON = @'
+{
+	"min_length" : "0",
+	"min_uppercase" : "0",
+	"min_lowercase" : "0",
+	"min_numeric" : "0",
+	"min_special" : "0",
+	"expires_after_x_days" : "999",
+	"different_from_previous_x_passwords" : "0",
+	"failed_logins_allowed" : "99"
+}
+'@ | ConvertFrom-Json
+
+$newJSON | ConvertTo-Json -Depth 20 | set-content 'C:\(PATH TO FILE)\newJSON_File.json'
